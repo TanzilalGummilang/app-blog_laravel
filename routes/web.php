b2856d7b9f6a6d5bb021c340ae3a8b01d18c239b
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-05-31 00:15:24 +0700

    bikin route dan view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+Route::fallback(function() {
+    return redirect('/');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => "Tanzilal Gummilang",
+        'profession' => "Web Developer",
+        'image' => "01.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
