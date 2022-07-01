7aba2b941191bd2065cfdddc51c6a6a4ad74e294
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-07-02 04:54:37 +0700

    authorization

---
+++ resources/views/dashboard/categories/index.blade.php
@@ -0,0 +1,66 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Post Category</h1>
+</div>
+
+<div class="col-6">
+
+  @if (session()->has('success'))
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  @endif
+
+  <a href="/dashboard/categories/create" class="btn btn-primary btn-sm">
+    <span data-feather="plus"></span> Create New Category
+  </a>
+
+  <div class="table-responsive mt-2">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Category Name</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach ($categories as $category)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $category->name }}</td>
+            <td>
+
+              <a href="/dashboard/categories/{{ $category->slug }}" class="badge bg-info">
+                <span data-feather="eye"></span>
+              </a>
+
+              <a href="/dashboard/categories/{{ $category->slug }}/edit" class="badge bg-warning">
+                <span data-feather="edit"></span>
+              </a>
+
+              <form action="/dashboard/categories/{{ $category->slug }}" method="POST" class="d-inline">
+                @method('delete')
+                @csrf
+
+                <button class="badge bg-danger border-0" onclick="return confirm('Delete this post?')">
+                  <span data-feather="x-circle"></span>
+                </button>
+              </form>
+
+            </td>
+          </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
@@ -61,3 +62,5 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('aut
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'createSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
